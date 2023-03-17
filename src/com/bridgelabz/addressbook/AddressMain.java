cc38231f9ab3d4ebc936c8471b9eaf68cef223c4
namratapatil1957 <namratapatil1957@gmail.com>
2023-03-17 09:27:00 +0530

    Ability to Edit the contact

--- src/com/bridgelabz/addressbook/AddressMain.java
+++ src/com/bridgelabz/addressbook/AddressMain.java
@@ -1,15 +1,37 @@
 package com.bridgelabz.addressbook;
 
+import java.util.Scanner;
+
 public class AddressMain {
 	
 	public static void main(String[] args){
+		
+		Scanner sc = new Scanner (System.in);
 		
 		ContactPerson Cp = new ContactPerson();
 
 		System.out.println(" --------- Enter the details in the Address Book --------- ");
 		System.out.println();
-		Cp.add();
+		
+		String action = null;
+		do {
+			System.out.println(" \n Enter what action you want to perform: ");
+			System.out.println(" \n 1. Add  \n 2. Edit  \n 3. Exit");
+			action = sc.next();
+			
+			switch (action) {
+			
+			case "add":
+				Cp.add();
+				break;
+				
+			case "edit":
+				Cp.edit();
+				break;
+			
+			}
+		}while(action != "exit");
 		
 	}
-
+			
 }
--- src/com/bridgelabz/addressbook/ContactPerson.java
+++ src/com/bridgelabz/addressbook/ContactPerson.java
@@ -10,11 +10,9 @@ public class ContactPerson {
 	AddressBook[] arr = new AddressBook[10];
 	
 	int i = 0;
-	int n = 2;
 
 	void add(){
 		
-		for (int i = 0; i < n; i++) {
 			System.out.println(" Enter the first name of the person: ");
 			String fname = sc.next();
 			person.setFname(fname);
@@ -47,13 +45,70 @@ public class ContactPerson {
 			String email = sc.next();
 			person.setEmail(email);
 		
-			arr[i] = person;
+			arr[0] = person;
 			
 			for (int j = 0; j < arr.length; j++) {
 				if (arr[j] == null)
 					break;
 				System.out.println(arr[i]);
 			}	
-		}	
+			
 	}
+	
+
+	void edit() {
+		
+		System.out.println(" Enter the first name of the person: ");
+		String name = sc.next();
+		person.setFname(name);
+		
+		if(name.equals(person.getFname())) {
+			
+		    System.out.println("Enter the number for editing: ");
+		    System.out.println(" 1: Last name /n 2:Address /n 3:City /n 4:State /n 5:Phone_no /n 6:Zip ");
+		    int option = sc.nextInt();	               	
+		    	
+		    	switch (option) {
+			        case 1:
+			        	System.out.println("Enter the lastname");
+			            String lname = sc.next();
+			            person.setLname(lname);
+			            break;
+			            
+			        case 2:
+			        	System.out.println("Enter the address");                                
+			        	String address = sc.next();                                
+			        	person.setAddress(address);                                
+			        	break;
+			                            
+			        case 3:                               
+			        	System.out.println("Enter the city");                                
+			        	String city = sc.next();                                
+			        	person.setCity(city);                               
+			        	break;
+			                            
+			        case 4:                               
+			        	System.out.println("Enter the state");                               
+			        	String state = sc.next();                                
+			        	person.setState(state);                                
+			        	break;
+			                            
+			        case 5:                                
+			        	System.out.println("Enter the phone number");                               
+			        	long phone_no = sc.nextLong();                                
+			        	person.setPhone_no(phone_no);                                
+			        	break;
+			                            
+			        case 6:                               
+			        	System.out.println("Enter the zip");                               
+			        	int zip = sc.nextInt();                                
+			        	person.setZip(zip);                               
+			        	break;
+			        	}   
+		
+			        arr[0] = person;
+		        }
+		   
+	}
+	
 }
